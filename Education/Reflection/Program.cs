c9994b59c1b682cf24d422409e51ddd4f1327d9f
NIX\limansky <alexandr.limansky@nixsolutions.com>
2017-07-18 18:45:16 +0300

    Initial commit for Reflection Task

---
+++ Education/Reflection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using educationClasses = Education_dotNet_Reflection_classes;
+
+class MemStream
+{
+    static void Main()
+    {
+        Type resultType = null;
+        Assembly assembly = typeof(educationClasses.ClassA).Module.Assembly;
+        foreach (var type in assembly.GetTypes())
+        {
+            foreach (var i in type.GetInterfaces())
+            {
+                if (i.Name == "IInterface")
+                {
+                    resultType = type;
+                }
+            }
+        }
+        if (resultType != null)
+        {
+            object obj = Activator.CreateInstance(resultType);
+            MethodInfo currentIndexMethod = resultType.GetMethod("set_CurrentIndex");
+            MethodInfo getNextIndexMethod = resultType.GetMethod("GetNextIndex");
+            currentIndexMethod.Invoke(obj, new object[] { 32 });
+            object result = getNextIndexMethod.Invoke(obj, null);
+            Console.WriteLine((result));
+        }
+        Console.ReadKey();
+    }
+}
