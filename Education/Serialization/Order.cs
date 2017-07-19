9280bedda7074feaa8fdf9b17892ab5343c1e0f6
NIX\limansky <alexandr.limansky@nixsolutions.com>
2017-07-19 14:36:35 +0300

    Added completed task about serialization

---
+++ Education/Serialization/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Order : ISerializable
+    {
+        public int Count;
+
+        public decimal Price;
+        [NonSerialized]
+        public decimal TotalPrice;
+
+        public Order(int count, decimal price)
+        {
+            Count = count;
+            Price = price;
+            TotalPrice = count * price;
+        }
+
+        public Order(SerializationInfo info, StreamingContext context)
+        {
+            Count = (int)info.GetValue("Count", typeof(int));
+            Price = (decimal)info.GetValue("Price", typeof(decimal));
+            TotalPrice = Count * Price;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Count", Count, typeof(int));
+            info.AddValue("Price", Price, typeof(decimal));
+        }
+    }
+}
