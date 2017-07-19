b5ed61d873e9b5d99c43aca02299eef245d34588
NIX\limansky <alexandr.limansky@nixsolutions.com>
2017-07-19 10:43:30 +0300

    Made changes to clear and fix code

--- Education/Reflection/Program.cs
+++ Education/Reflection/Program.cs
@@ -1,32 +1,75 @@
 ﻿using System;
 using System.Reflection;
-using educationClasses = Education_dotNet_Reflection_classes;
 
 class MemStream
 {
     static void Main()
     {
-        Type resultType = null;
-        Assembly assembly = typeof(educationClasses.ClassA).Module.Assembly;
-        foreach (var type in assembly.GetTypes())
+        Type outInterface, classImplementedInterface;
+        outInterface = GetOuterInterface();
+        classImplementedInterface = GetClassImplementingInterface(outInterface);
+        if (classImplementedInterface != null)
+        {
+            object obj = Activator.CreateInstance(classImplementedInterface);
+            Console.WriteLine("Enter the current index (number): ");
+            bool isInputCorrect;
+            int indexFromInput = 0;
+            isInputCorrect = Int32.TryParse(Console.ReadLine(), out indexFromInput);
+            if(isInputCorrect)
+            {
+                SetCurrentIndex(obj, indexFromInput);
+                Console.WriteLine("Next index is: ");
+                Console.WriteLine((GetNextIndex(obj)));
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+        }
+        Console.ReadKey();
+    }
+
+    static Type GetOuterInterface()
+    {
+        Assembly outAssembly = Assembly.LoadFrom("d:\\Education_dotNet_Reflection_interface.dll");
+        Type result = null;
+        foreach (var type in outAssembly.GetTypes())
+        {
+            if (type.Name == "IInterface")
+            {
+                result = type;
+            }
+        }
+        return result;
+    }
+
+    static Type GetClassImplementingInterface(Type outInterface)
+    {
+        Assembly outAssembly = Assembly.LoadFrom("d:\\Education_dotNet_Reflection_classes.dll");
+        Type result = null;
+        foreach (var type in outAssembly.GetTypes())
         {
             foreach (var i in type.GetInterfaces())
             {
-                if (i.Name == "IInterface")
+                if (i == outInterface)
                 {
-                    resultType = type;
+                    result = type;
                 }
             }
         }
-        if (resultType != null)
-        {
-            object obj = Activator.CreateInstance(resultType);
-            MethodInfo currentIndexMethod = resultType.GetMethod("set_CurrentIndex");
-            MethodInfo getNextIndexMethod = resultType.GetMethod("GetNextIndex");
-            currentIndexMethod.Invoke(obj, new object[] { 32 });
-            object result = getNextIndexMethod.Invoke(obj, null);
-            Console.WriteLine((result));
-        }
-        Console.ReadKey();
+        return result;
+    }
+
+    static void SetCurrentIndex(object classInstance, int valueToSet)
+    {
+        PropertyInfo currentIndexProperty = classInstance.GetType().GetProperty("CurrentIndex");
+        currentIndexProperty.SetValue(classInstance, valueToSet);
+    }
+
+    static int GetNextIndex(object classInstance)
+    {
+        MethodInfo getNextIndexMethod = classInstance.GetType().GetMethod("GetNextIndex");
+        object result = getNextIndexMethod.Invoke(classInstance, null);
+        return Convert.ToInt32(result);
     }
 }
