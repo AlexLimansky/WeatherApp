7f2091dc54235a96f2af35c90705b6fef9e062b7
NIX\limansky <alexandr.limansky@nixsolutions.com>
2017-07-20 12:10:55 +0300

    Added completed Multithreading task

---
+++ Education/Multithreading/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace Multithreading
+{
+    public delegate string DisplayHandler(string path);
+    class Program
+    {
+        static Timer timer;
+        static string previousText, currentText, result;
+        static void Main(string[] args)
+        {
+            previousText = "";
+            currentText = "";
+            string pathToFile = "test.txt";
+            using (StreamReader sr = new StreamReader(pathToFile))
+            {
+                currentText = sr.ReadToEnd();
+            }
+            DisplayHandler handler = new DisplayHandler(StartTimer);
+            Console.WriteLine("Changes check is on");
+            IAsyncResult resultObj = handler.BeginInvoke(pathToFile, new AsyncCallback(AsyncCompleted), null);
+            string res = handler.EndInvoke(resultObj);
+            Console.ReadLine();
+        }
+        public static void Check(object obj)
+        {
+            previousText = currentText;
+            string path = (string)obj;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                currentText = sr.ReadToEnd();
+                if (currentText != previousText)
+                {
+                    result = currentText;
+                    Console.WriteLine("Last saved text:" + Environment.NewLine + previousText);
+                    Console.WriteLine("Current text:");
+                    Console.WriteLine(result);
+                    Console.WriteLine("Changes check is off");
+                    timer.Dispose();
+                }
+            }
+        }
+        static string StartTimer(string path)
+        {
+            TimerCallback tm = new TimerCallback(Check);
+            timer = new Timer(tm, path, 0, 1000);
+            return result;
+        }
+        static void AsyncCompleted(IAsyncResult resObj)
+        {
+            string message = (string)resObj.AsyncState;
+            Console.WriteLine(message);
+        }
+    }
+}
