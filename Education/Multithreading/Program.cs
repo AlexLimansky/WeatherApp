ab492cbfce05abfa9b02e04e30070fb1e4f32855
NIX\limansky <alexandr.limansky@nixsolutions.com>
2017-07-20 15:28:44 +0300

    Placed logic to delegate & made timer a simple worker

--- Education/Multithreading/Program.cs
+++ Education/Multithreading/Program.cs
@@ -4,52 +4,45 @@ using System.IO;
 
 namespace Multithreading
 {
-    public delegate string DisplayHandler(string path);
+    public delegate void DisplayHandler(string path);
     class Program
     {
-        static Timer timer;
-        static string previousText, currentText, result;
+        static string previousText;
+        static object locker = new object();
         static void Main(string[] args)
         {
-            previousText = "";
-            currentText = "";
             string pathToFile = "test.txt";
             using (StreamReader sr = new StreamReader(pathToFile))
             {
-                currentText = sr.ReadToEnd();
-            }
-            DisplayHandler handler = new DisplayHandler(StartTimer);
-            Console.WriteLine("Changes check is on");
-            IAsyncResult resultObj = handler.BeginInvoke(pathToFile, new AsyncCallback(AsyncCompleted), null);            
+                previousText = sr.ReadToEnd();
+            }           
+            TimerCallback tm = new TimerCallback(Check);
+            Timer timer = new Timer(tm, pathToFile, 0, 1000);          
             Console.ReadLine();
         }
         public static void Check(object obj)
         {
-            previousText = currentText;
             string path = (string)obj;
-            using (StreamReader sr = new StreamReader(path))
+            DisplayHandler handler = new DisplayHandler(DisplayDifference);
+            IAsyncResult resultObj = handler.BeginInvoke(path, null, null);
+        }
+        static void DisplayDifference(string path)
+        {
+            lock(locker)
             {
-                currentText = sr.ReadToEnd();
-                if (currentText != previousText)
+                using (StreamReader sr = new StreamReader(path))
                 {
-                    result = currentText;
-                    Console.WriteLine("File was changed " + DateTime.Now.ToShortDateString() + " at " + DateTime.Now.ToShortTimeString());
-                    Console.WriteLine("Last saved text:" + Environment.NewLine + previousText);
-                    Console.WriteLine("Current text:");
-                    Console.WriteLine(result + Environment.NewLine);
+                    string currentText = sr.ReadToEnd();
+                    if (currentText != previousText)
+                    {
+                        Console.WriteLine("File was changed " + DateTime.Now.ToShortDateString() + " at " + DateTime.Now.ToShortTimeString());
+                        Console.WriteLine("Last saved text:" + Environment.NewLine + previousText);
+                        Console.WriteLine("Current text:");
+                        Console.WriteLine(currentText + Environment.NewLine);
+                    }
+                    previousText = currentText;
                 }
-            }
-        }
-        static string StartTimer(string path)
-        {
-            TimerCallback tm = new TimerCallback(Check);
-            timer = new Timer(tm, path, 0, 1000);
-            return result;
-        }
-        static void AsyncCompleted(IAsyncResult resObj)
-        {
-            string message = (string)resObj.AsyncState;
-            Console.WriteLine(message);
+            }           
         }
     }
 }
