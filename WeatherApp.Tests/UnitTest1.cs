2125f10fe6c837ce23210ae4b79b7146be43e3aa
NIX\limansky <alexandr.limansky@nixsolutions.com>
2017-09-20 18:22:10 +0300

    Added passed test for experiment

--- WeatherApp.Tests/UnitTest1.cs
+++ WeatherApp.Tests/UnitTest1.cs
@@ -12,17 +12,24 @@ namespace WeatherApp.Tests
 {
     public class UnitTest1
     {
+        private Mock<UserManager<ApplicationUser>> GetMockUserManager()
+        {
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            return new Mock<UserManager<ApplicationUser>>(
+                userStoreMock.Object, null, null, null, null, null, null, null, null);
+        }
+
         [Fact]
         public void Test1()
         {
             var manMock = new Mock<IWeatherManager>();
             var userMock = new Mock<IdentityUser>();
-            var usersMock = new Mock<UserManager<ApplicationUser>>();
+            var usersMock = GetMockUserManager();
             var locMock = new Mock<IStringLocalizer<WeatherController>>();
 
             WeatherController item = new WeatherController(manMock.Object, usersMock.Object, locMock.Object);
 
-            Assert.IsType(typeof(int), item);
+            Assert.IsType(typeof(WeatherController), item);
         }
     }
 }
