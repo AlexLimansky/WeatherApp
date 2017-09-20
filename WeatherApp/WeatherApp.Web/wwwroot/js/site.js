282bc2b28c799b726fbb91025ab05936eb98dc68
NIX\limansky <alexandr.limansky@nixsolutions.com>
2017-09-20 14:40:13 +0300

    asdasd

--- WeatherApp/WeatherApp.Web/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $('#myModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);//Button which is clicked
-        var clickedButtonId = button.data('id');//Get id of the button
-        // set id to the hidden input field in the form.
-        $("#CityName").text(clickedButtonId);
-        $("#city").val(clickedButtonId);
-    });
-});
